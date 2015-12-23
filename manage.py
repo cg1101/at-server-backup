dffed89dc2aecf191e7107d3bb935d3856fd001d
gcheng <gcheng@appen.com>
2015-12-23 13:29:41 +1100

    added manager command list to show handler list

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import os
+
 from app import create_app
 from flask.ext.script import Manager
 
@@ -13,6 +15,12 @@ manager = Manager(app)
 # 	return dict(app=app, db=db, User=User, Role=Role)
 # manager.add_command('shell', Shell(make_context=make_shell_context))
 # manager.add_command('db', MigrateCommand)
+
+@manager.command
+def list():
+	for rule in app.url_map.iter_rules():
+		print(str(rule), 'methods:', ','.join(rule.methods), '->', rule.endpoint)
+
 
 if __name__ == "__main__":
 	manager.run()
