79ebb50feb647038cdc6a10ebfc7c7406fef6ce3
gcheng <gcheng@appen.com>
2016-01-06 14:05:07 +1100

    added NON_ADMIN_REDIRECT_URL constant in config or proper redirection

--- config.py
+++ config.py
@@ -5,37 +5,37 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-	FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 	@staticmethod
 	def init_app(app):
 		pass
 
 class DevelopmentConfig(Config):
 	DEBUG = True
-	MAIL_SERVER = 'smtp.googlemail.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	AUTHENTICATION_LOGIN_URL = 'http://localhost:8080'
-	AUTHENTICATION_LOGOUT_URL = 'http://localhost:8080/logout'
+	# MAIL_SERVER = 'smtp.googlemail.com'
+	# MAIL_PORT = 587
+	# MAIL_USE_TLS = True
+	# MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	# MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 		'postgresql://localhost/atdb'
+	NON_ADMIN_REDIRECT_URL = 'http://appenonline.appen.com.au'
+	AUTHENTICATION_LOGIN_URL = 'http://localhost:8080'
+	AUTHENTICATION_LOGOUT_URL = 'http://localhost:8080/logout'
 
 class TestingConfig(Config):
 	TESTING = True
-	AUTHENTICATION_LOGIN_URL = 'http://localhost:8080'
-	AUTHENTICATION_LOGOUT_URL = 'http://localhost:8080/logout'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
 		'postgresql://localhost/at_test'
+	NON_ADMIN_REDIRECT_URL = 'http://appenonline.appen.com.au'
+	AUTHENTICATION_LOGIN_URL = 'http://localhost:8080'
+	AUTHENTICATION_LOGOUT_URL = 'http://localhost:8080/logout'
 
 class ProductionConfig(Config):
-	AUTHENTICATION_LOGIN_URL = 'http://appenonline.appen.com.au'
-	AUTHENTICATION_LOGOUT_URL = 'http://appenonline.appen.com.au/logout'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'postgresql://dbserver/appentext'
+	NON_ADMIN_REDIRECT_URL = 'http://appenonline.appen.com.au'
+	AUTHENTICATION_LOGIN_URL = 'http://appenonline.appen.com.au'
+	AUTHENTICATION_LOGOUT_URL = 'http://appenonline.appen.com.au/logout'
 
 config = {
 	'development': DevelopmentConfig,
