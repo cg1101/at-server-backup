cfa5b4e6b7234bf763e1938c229c83f100923b2f
gcheng-appen <gcheng@appen.com>
2016-06-29 16:41:20 +1000

    added cors to allow accessing apis from a different website

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask, session, request, send_file, after_this_request,\
 		redirect, jsonify, make_response
+from flask.ext.cors import CORS
 
 from config import config
 from db.model import User
@@ -17,12 +18,13 @@ def create_app(config_name):
 	app.config.from_object(config[config_name])
 	config[config_name].init_app(app)
 	db.init_app(app)
+	CORS(app, resources={'/api/1.0/*': {'origins': '*'}})
 
-	app.wsgi_app = MyAuthMiddleWare(app.wsgi_app,
-		app.config['AUTHENTICATION_LOGIN_URL'],
-		public_prefixes=['/static/', '/webservices', '/logout'],
-		json_prefixes=['/api/'],
-	)
+	# app.wsgi_app = MyAuthMiddleWare(app.wsgi_app,
+	# 	app.config['AUTHENTICATION_LOGIN_URL'],
+	# 	public_prefixes=['/static/', '/webservices', '/logout'],
+	# 	json_prefixes=['/api/'],
+	# )
 
 	from app.api import api_1_0
 	# from app.webservices import webservices
