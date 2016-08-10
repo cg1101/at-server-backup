de5f2d05405acc46a1cef4cd5924a68478a73b32
gcheng-appen <gcheng@appen.com>
2016-08-10 16:38:14 +1000

    added filehandler to support utts loading.

--- app/util/filehandler.py
+++ app/util/filehandler.py
@@ -2,6 +2,7 @@
 import re
 import functools
 import cgi
+import json
 
 from app.i18n import get_text as _
 
@@ -31,15 +32,29 @@ class FileHandler(object):
 	def load_data(self, file, **options):
 		return self.func(file, **options)
 
+
 def count_words(text):
 	return len(text.split())
 
+
+def smart(fn):
+	@functools.wraps(fn)
+	def smart_loader(file, *args, **kwargs):
+		close_on_exit = False
+		if isinstance(file, basestring):
+			fname = file
+			file = open(fname)
+			close_on_exit = True
+		try:
+			return fn(file, *args, **kwargs)
+		finally:
+			if close_on_exit:
+				file.close()
+	return smart_loader
+
+
+@smart
 def load_generic_text_file(file, split=False, escape=True, skip_header_line=0, data_column=1, delimiter=None, **kwargs):
-	close_on_exit = False
-	if isinstance(file, basestring):
-		fname = file
-		file = open(fname)
-		close_on_exit = True
 	# normalize/validate options
 	split = bool(split)
 	escape = bool(escape)
@@ -74,49 +89,57 @@ def load_generic_text_file(file, split=False, escape=True, skip_header_line=0, d
 		html_safe = lambda (x): x
 
 	itemDicts = []
-	try:
-		for i, l in enumerate(file):
-			if i < skip_header_line:
-				continue
-			l = unicode(l.rstrip('\r\n'), 'utf-8')
-			data = get_data(l)
-			itemDicts.append({
-				'rawText': html_safe(data),
-				'words': count_words(data),
-			})
-		return itemDicts
-	finally:
-		if close_on_exit:
-			file.close()
+	for i, l in enumerate(file):
+		if i < skip_header_line:
+			continue
+		l = unicode(l.rstrip('\r\n'), 'utf-8')
+		data = get_data(l)
+		itemDicts.append({
+			'rawText': html_safe(data),
+			'words': count_words(data),
+		})
+	return itemDicts
 
 
+@smart
 def load_tdf_file(file):
-	close_on_exit = False
-	if isinstance(file, basestring):
-		fname = file
-		file = open(fname)
-		close_on_exit = True
 	itemDicts = []
-	try:
-		for i, l in enumerate(file):
-			if i == 0 and l.startswith('file\t'):
-				continue
-			fields = l.split('\t')
-			if len(fields) != 14:
-				raise ValueError(_('invalid format of input tdf file'))
-			data = fields[7]
-			itemDicts.append({
-				'rawText': cgi.escape(data),
-				'words': count_words(data),
-			})
-		return itemDicts
-	finally:
-		if close_on_exit:
-			file.close()
+	for i, l in enumerate(file):
+		if i == 0 and l.startswith('file\t'):
+			continue
+		fields = l.split('\t')
+		if len(fields) != 14:
+			raise ValueError(_('invalid format of input tdf file'))
+		data = fields[7]
+		itemDicts.append({
+			'rawText': cgi.escape(data),
+			'words': count_words(data),
+		})
+	return itemDicts
+
+
+@smart
+def load_utt_file(file):
+	itemDicts = []
+	jsonDict = json.load(file)
+	# jsonDict['taskId']
+	utts = jsonDict['utterances']
+	for utt in utts:
+		meta = {}
+		for key in ('filePath', 'audioSpec', 'audioDataLocation'):
+			meta[key] = utt[key]
+		itemDicts.append(dict(
+			rawText='',
+			hypothesis=utt['hypothesis'],
+			assemblyContext=utt['url'],
+			meta=json.dumps(meta),
+		))
+	return itemDicts
 
 
 FileHandler.register_handler('plaintext', load_generic_text_file)
 FileHandler.register_handler('tdf', load_tdf_file)
+FileHandler.register_handler('utts', load_utt_file)
 
 
 def get_handler(name):
