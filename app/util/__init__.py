e75f8fe5f9890c2d87041ab0b72c9722fc235da1
CHENG, Gang <gcheng@appen.com>
2017-04-10 12:02:40 +1000

    added CountryRatioLookupTable

--- app/util/__init__.py
+++ app/util/__init__.py
@@ -231,3 +231,20 @@ class PolicyChecker(object):
 		# 	).format(subTask.getPolicy, subTask.subTaskId)
 		return None
 
+
+class CountryRatioLookupTable(object):
+	def _populate_retio_map(iso2='AU'):
+		c = m.Country.query.filter(m.Country.iso2==iso2).one()
+		if not (c and c.hourlyRate != None and c.hourlyRate > 0):
+			raise RuntimeError('error loading reference hourly rate: {}'.format(iso2))
+		reference_rate = float(c.hourlyRate)
+		ratio_map = {}
+		countries = m.Country.query.all()
+		for c in countries:
+			ratio_map[c.countryId] = c.hourlyRate / reference_rate
+		return reference_rate, ratio_map
+	reference_rate, _ratio_map = _populate_retio_map()
+	@classmethod
+	def get_ratio(cls, countryId):
+		return cls._ratio_map.get(countryId)
+
