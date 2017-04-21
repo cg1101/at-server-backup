5dfa6ebba7cd0e797cb0005a88bd282a2f9efa01
CHENG, Gang <gcheng@appen.com>
2017-04-21 12:52:42 +1000

    test of including client repo

--- travis-ci/pre_build.sh
+++ travis-ci/pre_build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-DIR=$(cd `dirname ${0}`/../)
+# /home/travis/build/Appen/at_server
 
-# /home/travis/at_server
+CLIENT_DIR=$HOME/build/Appen/appen_next_client
+
+git clone --depth=50 --branch=qa git@github.com:Appen/appen_next_client.git ${CLIENT_DIR}
+pushd ${CLIENT_DIR}
+npm install
+bower install
+pushd lib
+for i in ../bower_components/*; do
+	ln -sf "$i"
+done
+ln -sf jquery jquery-2.2.3
+popd
+mkdir -p app build
+ln -sf "../build/dev/index.html" app
+ln -sf "../build/dev/static" app
+popd
+ln -sf "$CLIENT_DIR/app/index.html" app
+ln -sf "$CLIENT_DIR/app/static" app
+ls -l app
